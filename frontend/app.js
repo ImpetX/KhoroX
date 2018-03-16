05bc58293cd2cccd4a64b97b2d91070033c5028c
Kamal Sharif <ksharifbd@gmail.com>
2018-03-16 11:18:48 +0600

    Added app.js and removed index.html [closes #14]

---
+++ frontend/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+let app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', `${__dirname}/views`);
+
+app.listen(app.get('port'), () => {
+    console.log(`Server up: http://localhost:${app.get('port')}`);
+});
