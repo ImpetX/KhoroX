bc8e2844ed2cb91fa142dc5ac086991e410a12da
Kamal Sharif <ksharifbd@gmail.com>
2018-03-27 11:40:13 +0600

    Implemented 404 error handling [closes #17]

--- frontend/app.js
+++ frontend/app.js
@@ -7,15 +7,33 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-app.get('/about', (req, res) => {
-    res.render('index');
-});
-
 app.use('/public', express.static(path.join(__dirname, '/public')));
+
+app.get('(/|/about)', (req, res) => {
+    res.render('index');
+});
+
+app.get('*', (req, res, next) => {
+    const err = new Error((`${req.ip} tried to reach ${req.originalUrl}`));
+    
+    err.statusCode = 404;
+    err.shouldRedirect = true;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.message);
+
+    if (!err.statusCode) {
+        err.statusCode = 500;
+    }
+
+    if (err.shouldRedirect) {
+        res.render('error');
+    } else {
+        console.error(err.statusCode);
+    }
+});
 
 app.listen(app.get('port'), () => {
     console.log(`Server up: http://localhost:${app.get('port')}`);
