4deb8a389a1fccc956257d60d42ef6b2fb9f0318
ksharifbd <ksharifbd@gmail.com>
2018-11-27 12:47:14 +0600

    adds userschema and instance methods

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,1 +1,22 @@
-// users schema goes here
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+const {Schema} = mongoose;
+
+const UserSchema = new Schema({
+    email: string,
+    hash: string,
+    salt: string,
+});
+
+UserSchema.methods.setPassword  = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
+
+UserSchema.methods.validatePassword = function(password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+
+    return this.hash === hash;
+};
