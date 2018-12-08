1ddfa1fa507f31d294b8ed52ac678ff365b4faf7
ksharifbd <ksharifbd@gmail.com>
2018-12-08 15:32:04 +0600

    renames route

--- backend/server/routes.js
+++ backend/server/routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
-const loginRoute = require('../modules/auth/routes');
+const authRoute = require('../modules/auth/routes');
 const expenseRoute= require('../modules/expense/routes');
+
 const router = express.Router();
 
 module.exports = app => {
-    router.use(loginRoute);
+    router.use(authRoute);
     router.use(expenseRoute);
 
     app.use(router);
