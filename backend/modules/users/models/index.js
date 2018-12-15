850b76c0f83711a316e37984637ec8a2cce18ee8
ksharifbd <ksharifbd@gmail.com>
2018-12-15 19:27:26 +0600

    removes noise

--- backend/modules/users/models/index.js
+++ backend/modules/users/models/index.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const crypto = require('crypto');
-const jwt = require('jsonwebtoken');
 
 const {Schema} = mongoose;
 
@@ -17,32 +15,5 @@ const UserSchema = new Schema({
         required: true,
     },
 });
-
-UserSchema.methods.validatePassword = function(password) {
-    const hashedPassword = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
-
-    return this.password === hashedPassword;
-};
-
-UserSchema.methods.generateJWT = function() {
-    const today = new Date();
-    const expirationDate = new Date(today);
-
-    expirationDate.setDate(today.getDate() + 60);
-
-    return jwt.sign({
-        email: this.email,
-        id: this._id,
-        exp: parseInt(expirationDate.getTime() / 1000, 10),
-    }, 'secret');
-};
-
-UserSchema.methods.toAuthJSON = function() {
-    return {
-        _id: this._id,
-        email: this.email,
-        token: this.generateJWT(),
-    };
-};
 
 module.exports = mongoose.model('User', UserSchema);
