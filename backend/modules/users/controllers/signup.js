88a3abb1dc91ee75deadc46ca3d10cf963f3d9f5
ksharifbd <ksharifbd@gmail.com>
2018-12-15 22:50:06 +0600

    restrics duplicate email records

--- backend/modules/users/controllers/signup.js
+++ backend/modules/users/controllers/signup.js
@@ -7,35 +7,48 @@ const generateJWT = require('./generateJWT');
 function signup(req, res) {
     const {email, password} = req.body;
 
-    bcrypt
-        .hash(password, 10)
-        .then(hash => {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email,
-                password: hash,
-            });
-
-            return user
-                .save()
-                .then(result => {
-                    const {id, email} = result;
-                    res
-                        .status(201)
-                        .json({
-                            user: {
-                                _id: id,
-                                email,
-                                token: generateJWT(id, email),
-                            },
+    User
+        .find({email})
+        .exec()
+        .then(user => {
+            if(user.length >= 1){
+                return res
+                    .status(409)
+                    .json({
+                        duplicateEmail: true,
+                    });
+            } else {
+                bcrypt
+                    .hash(password, 10)
+                    .then(hash => {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email,
+                            password: hash,
                         });
-                });
-        })
-        .catch(err => res
-            .status(500)
-            .json({
-                error: err.message,
-            }));
+
+                        user
+                            .save()
+                            .then(result => {
+                                const {id, email} = result;
+                                res
+                                    .status(201)
+                                    .json({
+                                        user: {
+                                            _id: id,
+                                            email,
+                                            token: generateJWT(id, email),
+                                        },
+                                    });
+                            });
+                    })
+                    .catch(err => res
+                        .status(500)
+                        .json({
+                            error: err.message,
+                        }));
+            }
+        });
 }
 
 module.exports = signup;
