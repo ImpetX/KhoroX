452da839e9675b5b134e7bcee5db02d1334072d6
ksharifbd <ksharifbd@gmail.com>
2018-12-24 11:20:13 +0600

    refactors server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -22,7 +23,26 @@ mongoose.connection.on('open', () => {
     console.log(`mongoose connected at ${config.DB_URL}`);
 });
 
-app.listen(app.get('port'), () => {
+const server = http.createServer(app);
+
+const boot = () => {
+    server.listen(app.get('port'), () => {
+        // eslint-disable-next-line no-console
+        console.log(`Server up: http://localhost:${app.get('port')}`);
+    });
+};
+
+const shutdown = () => {
+    server.close(process.exit);
+};
+
+if(require.main === module) {
+    boot();
+} else {
     // eslint-disable-next-line no-console
-    console.log(`Server up: http://localhost:${app.get('port')}`);
-});
+    console.info('running app for testing');
+
+    exports.boot = boot;
+    exports.shutdown = shutdown;
+    exports.port = app.get('port');
+}
