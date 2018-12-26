c30c024eda8e0b43ae4cdc7126931587e4797757
ksharifbd <ksharifbd@gmail.com>
2018-12-26 15:34:28 +0600

    adds before-after hooks

--- backend/modules/users/_tests_/generateJWT.spec.js
+++ backend/modules/users/_tests_/generateJWT.spec.js
@@ -1,1 +1,16 @@
-// tests for generateJWT.js goes here
+const generateJWT = require('../controllers/generateJWT');
+const server = require('../../../server');
+
+const {boot, shutdown} = server;
+
+describe('Generate JSON web token', () => {
+    beforeAll(() => {
+        boot();
+    });
+
+    // write tests here
+
+    afterAll(() => {
+        shutdown();
+    });
+});
