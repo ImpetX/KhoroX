b416e6cf9e6ef7dd67e6a02933a0a9fbf9f50b53
ksharifbd <ksharifbd@gmail.com>
2019-01-07 11:31:53 +0600

    attempts 1

--- backend/modules/users/_tests_/signup.spec.js
+++ backend/modules/users/_tests_/signup.spec.js
@@ -2,3 +2,25 @@ const signup = require('../controllers/signup');
 const server = require('../../../server');
 
 const {boot, shutdown} = server;
+
+describe('Sign up controller', () => {
+    beforeAll(async () => {
+        try {
+            await boot();
+        } catch (err) {
+            console.log('error...', err);
+        }
+    });
+
+    test('dummy test', () => {
+        expect(1).toEqual(1);
+    });
+
+    afterAll(async () => {
+        try {
+            await shutdown();
+        } catch (err) {
+            console.log('error...', err);
+        }
+    });
+});
