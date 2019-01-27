3a231f7cc96b827b1000e8ea198edf42259542b2
ksharifbd <ksharifbd@gmail.com>
2019-01-27 19:11:17 +0600

    spins up separate test db

--- backend/modules/users/_tests_/signup.spec.js
+++ backend/modules/users/_tests_/signup.spec.js
@@ -1,12 +1,21 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const signup = require('../controllers/signup');
 const app = require('../../../server');
+const {DB_URL_TEST} = require('../../../config/localhost.json');
 
 describe('Sign up controller', () => {
     let server;
 
-    beforeAll(done => {
+    beforeAll(async () => {
+        await mongoose.connect(DB_URL_TEST, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+        });
+    });
+
+    beforeEach(done => {
         server = http.createServer(app);
 
         server.listen(done);
@@ -16,7 +25,11 @@ describe('Sign up controller', () => {
         expect(1).toEqual(1);
     });
 
-    afterAll(done => {
+    afterEach(done => {
         server.close(done);
+    });
+
+    afterAll(done => {
+        mongoose.connection.close(done);
     });
 });
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const config = require('./config/localhost.json');
+const {DB_URL} = require('./config/localhost.json');
 const serverConfig = require('./server/configure');
 
 let app = express();
@@ -10,13 +10,13 @@ app.set('port', process.env.PORT || 3001);
 app.set('views', `${__dirname}/views`);
 
 if(require.main === module) {
-    mongoose.connect(config.DB_URL, {
+    mongoose.connect(DB_URL, {
         useNewUrlParser: true,
         useCreateIndex: true,
     })
         .then(() => {
             // eslint-disable-next-line no-console
-            console.log(`mongoose connected at ${config.DB_URL}`);
+            console.log(`mongoose connected at ${DB_URL}`);
         }).catch(err => {
             // eslint-disable-next-line no-console
             console.error(err);
